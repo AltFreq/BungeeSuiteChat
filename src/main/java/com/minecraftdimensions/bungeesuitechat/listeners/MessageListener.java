ee1188f209bc3c75b4780ac2d250c236d8452943
bloodsplat <pure__instinct@hotmail.com>
2013-09-05 22:33:30 +1000

    Change the way affixes are sent and loaded by the server

--- src/main/java/com/minecraftdimensions/bungeesuitechat/listeners/MessageListener.java
+++ src/main/java/com/minecraftdimensions/bungeesuitechat/listeners/MessageListener.java
@@ -1,142 +1,134 @@
 package com.minecraftdimensions.bungeesuitechat.listeners;
 
-import java.io.ByteArrayInputStream;
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
-import org.bukkit.permissions.Permission;
-import org.bukkit.permissions.PermissionDefault;
-import org.bukkit.plugin.messaging.PluginMessageListener;
-
 import com.minecraftdimensions.bungeesuitechat.BungeeSuiteChat;
 import com.minecraftdimensions.bungeesuitechat.managers.ChannelManager;
 import com.minecraftdimensions.bungeesuitechat.managers.PlayerManager;
 import com.minecraftdimensions.bungeesuitechat.managers.PrefixSuffixManager;
 import com.minecraftdimensions.bungeesuitechat.objects.BSPlayer;
 import com.minecraftdimensions.bungeesuitechat.objects.ServerData;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionDefault;
+import org.bukkit.plugin.messaging.PluginMessageListener;
+
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class MessageListener implements PluginMessageListener {
 
-	@Override
-	public void onPluginMessageReceived(String pluginChannel, Player reciever,byte[] message) {
-		if (!pluginChannel.equalsIgnoreCase(BungeeSuiteChat.INCOMING_PLUGIN_CHANNEL))
-			return;
-		DataInputStream in = new DataInputStream(new ByteArrayInputStream(
-				message));
-		String channel = null;
-		try {
-			channel = in.readUTF();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		if(channel.equals("SendGlobalChat")){
-			try {
-				ChannelManager.getGlobalChat(in.readUTF(),in.readUTF());
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return;
-		}
-		if(channel.equals("SendAdminChat")){
-			try {
-				ChannelManager.getAdminlChat(in.readUTF());
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return;
-		}
-		if(channel.equals("SendPlayer")){
-			try {
-				PlayerManager.addPlayer(new BSPlayer(in.readUTF()));
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return;
-		}
-		if(channel.equals("SendChannel")){
-			try {
-				ChannelManager.addChannel(in.readUTF());
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return;
-		}
-		if(channel.equals("SendServerData")){
-			try {
-				ServerData.deserialise(in.readUTF());
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return;
-		}
-		if(channel.equals("PrefixesAndSuffixes")){
-			String prefix[] = null;
-			String suffix[] = null;
-			try {
-				prefix = in.readUTF().split("%");
-				suffix = in.readUTF().split("%");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			if(prefix.length>1){
-				for(int i=0;i<prefix.length;i+=2){
-					PrefixSuffixManager.prefixes.put(prefix[i], prefix[i+1]);
-					Bukkit.getPluginManager().addPermission(new Permission("bungeesuite.chat.prefix."+prefix[i],"Gives access to the "+prefix[i+1]+" prefix", PermissionDefault.FALSE));
-				}
-				PrefixSuffixManager.prefix = true;
-			}
-			if(suffix.length>1){
-				for(int i=0;i<suffix.length;i+=2){
-					PrefixSuffixManager.suffixes.put(suffix[i], suffix[i+1]);
-					Bukkit.getPluginManager().addPermission(new Permission("bungeesuite.chat.suffix."+suffix[i],"Gives access to the "+suffix[i+1]+" suffix", PermissionDefault.FALSE));
-				}
-				PrefixSuffixManager.suffix = true;
-			}
-			for(Permission p :Bukkit.getPluginManager().getPermissions()){
-				if(p.getName().startsWith("bungeesuite")){
-				System.out.println(p.getName()+" default= " + p.getDefault().toString());
-				}
-			}
-		}
-		if(channel.equals("SendPlayersIgnores")){
-			String player = null;
-			String ignoresString[] = null ;
-			try {
-				player = in.readUTF();
-				ignoresString = in.readUTF().split("%");
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			final ArrayList<String> ignores = new ArrayList<String>();
-			for(String s : ignoresString){
-				ignores.add(s);
-			}
-			final String name = player;
-			BSPlayer p = PlayerManager.getPlayer(player);
-			if(p!=null){
-			p.setIgnores(ignores);
-			}else{
-				Bukkit.getScheduler().runTaskLaterAsynchronously(BungeeSuiteChat.instance, new Runnable(){
+    @Override
+    public void onPluginMessageReceived( String pluginChannel, Player reciever, byte[] message ) {
+        if ( !pluginChannel.equalsIgnoreCase( BungeeSuiteChat.INCOMING_PLUGIN_CHANNEL ) )
+            return;
+        DataInputStream in = new DataInputStream( new ByteArrayInputStream( message ) );
+        String channel = null;
+        try {
+            channel = in.readUTF();
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        }
+        if ( channel.equals( "SendGlobalChat" ) ) {
+            try {
+                ChannelManager.getGlobalChat( in.readUTF(), in.readUTF() );
+            } catch ( IOException e ) {
+                e.printStackTrace();
+            }
+            return;
+        }
+        if ( channel.equals( "SendAdminChat" ) ) {
+            try {
+                ChannelManager.getAdminlChat( in.readUTF() );
+            } catch ( IOException e ) {
+                e.printStackTrace();
+            }
+            return;
+        }
+        if ( channel.equals( "SendPlayer" ) ) {
+            try {
+                PlayerManager.addPlayer( new BSPlayer( in.readUTF() ) );
+            } catch ( IOException e ) {
+                e.printStackTrace();
+            }
+            return;
+        }
+        if ( channel.equals( "SendChannel" ) ) {
+            try {
+                ChannelManager.addChannel( in.readUTF() );
+            } catch ( IOException e ) {
+                e.printStackTrace();
+            }
+            return;
+        }
+        if ( channel.equals( "SendServerData" ) ) {
+            try {
+                ServerData.deserialise( in.readUTF() );
+            } catch ( IOException e ) {
+                e.printStackTrace();
+            }
+            return;
+        }
+        if ( channel.equals( "PrefixesAndSuffixes" ) ) {
 
-					@Override
-					public void run() {
-						PlayerManager.getPlayer(name).setIgnores(ignores);
-					}
-					
-				}, 10L);
-			}
-			return;
-		}
-		if(channel.equals("ReloadChat")){
-			ChannelManager.reload();
-			return;
-		}
+            boolean check;
+            String group;
+            String affix;
+            try {
+                check = in.readBoolean();
+                group = in.readUTF();
+                affix = in.readUTF();
+                if ( check ) {
+                    PrefixSuffixManager.prefixes.put( group, affix );
+                    PrefixSuffixManager.prefix = true;
+                    Bukkit.getPluginManager().addPermission( new Permission( "bungeesuite.chat.prefix." + group, "Gives access to the " + affix + " prefix", PermissionDefault.FALSE ) );
+                } else {
+                    PrefixSuffixManager.suffixes.put( group, affix );
+                    PrefixSuffixManager.suffix = true;
+                    Bukkit.getPluginManager().addPermission( new Permission( "bungeesuite.chat.suffix." + group, "Gives access to the " + affix + " suffix", PermissionDefault.FALSE ) );
+                }
+            } catch ( IOException e ) {
+                e.printStackTrace();
+            }
+
+        }
+        if ( channel.equals( "SendPlayersIgnores" ) ) {
+            String player = null;
+            String ignoresString[] = null;
+            try {
+                player = in.readUTF();
+                ignoresString = in.readUTF().split( "%" );
+            } catch ( IOException e ) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            final ArrayList<String> ignores = new ArrayList<String>();
+            for ( String s : ignoresString ) {
+                ignores.add( s );
+            }
+            final String name = player;
+            BSPlayer p = PlayerManager.getPlayer( player );
+            if ( p != null ) {
+                p.setIgnores( ignores );
+            } else {
+                Bukkit.getScheduler().runTaskLaterAsynchronously( BungeeSuiteChat.instance, new Runnable() {
+
+                    @Override
+                    public void run() {
+                        PlayerManager.getPlayer( name ).setIgnores( ignores );
+                    }
+
+                }, 10L );
+            }
+            return;
+        }
+        if ( channel.equals( "ReloadChat" ) ) {
+            ChannelManager.reload();
+            return;
+        }
 
 
-	}
+    }
 
 }
--- src/main/java/com/minecraftdimensions/bungeesuitechat/managers/PrefixSuffixManager.java
+++ src/main/java/com/minecraftdimensions/bungeesuitechat/managers/PrefixSuffixManager.java
@@ -1,117 +1,114 @@
 package com.minecraftdimensions.bungeesuitechat.managers;
 
-import java.util.HashMap;
-
+import com.minecraftdimensions.bungeesuitechat.BungeeSuiteChat;
 import org.bukkit.entity.Player;
 
-import com.minecraftdimensions.bungeesuitechat.BungeeSuiteChat;
+import java.util.HashMap;
 
 public class PrefixSuffixManager {
-	
-	public static HashMap<String, String> prefixes = new HashMap<String, String>();
-	public static HashMap<String, String> suffixes = new HashMap<String, String>();
-	public static boolean prefix;
-	public static boolean suffix;
-	
+
+    public static HashMap<String, String> prefixes = new HashMap<>();
+    public static HashMap<String, String> suffixes = new HashMap<>();
+    public static boolean prefix;
+    public static boolean suffix;
 
 
-	public static void reload() {
-		prefixes.clear();
-		suffixes.clear();
-	}
+    public static void reload() {
+        prefixes.clear();
+        suffixes.clear();
+    }
 
 
-	public static String getPlayersPermGroup(Player player) {
-		String group = BungeeSuiteChat.CHAT.getPrimaryGroup(player);
-		if(group == null){
-			return "";
-		}else{
-			return group;
-		}
+    public static String getPlayersPermGroup( Player player ) {
+        String group = BungeeSuiteChat.CHAT.getPrimaryGroup( player );
+        if ( group == null ) {
+            return "";
+        } else {
+            return group;
+        }
 
-	}
-
-
-	public static String getPlayersPermSuffix(Player player) {
-		String suffix = BungeeSuiteChat.CHAT.getPlayerSuffix(player);
-		if(suffix == null){
-			return "";
-		}
-		return suffix;
-	}
+    }
 
 
-	public static String getPlayersPermGroupSuffix(Player player) {
-		String group = BungeeSuiteChat.CHAT.getPrimaryGroup(player);
-		String suffix = "";
-		if(!group.equals("")){
-			suffix = BungeeSuiteChat.CHAT.getGroupSuffix(player.getWorld(), group);
-		}
-		if(suffix == null){
-			return "";
-		}
-		return suffix;
-	}
+    public static String getPlayersPermSuffix( Player player ) {
+        String suffix = BungeeSuiteChat.CHAT.getPlayerSuffix( player );
+        if ( suffix == null ) {
+            return "";
+        }
+        return suffix;
+    }
 
 
-	public static String getPlayersGroupPrefix(Player player) {
-		String group = BungeeSuiteChat.CHAT.getPrimaryGroup(player);
-		String prefix = "";
-		if(!group.equals("")){
-			prefix = BungeeSuiteChat.CHAT.getGroupPrefix(player.getWorld(), group);
-		}
-		if(prefix == null){
-			return "";
-		}
-		return prefix;
-	}
+    public static String getPlayersPermGroupSuffix( Player player ) {
+        String group = BungeeSuiteChat.CHAT.getPrimaryGroup( player );
+        String suffix = "";
+        if ( !group.equals( "" ) ) {
+            suffix = BungeeSuiteChat.CHAT.getGroupSuffix( player.getWorld(), group );
+        }
+        if ( suffix == null ) {
+            return "";
+        }
+        return suffix;
+    }
 
 
-	public static CharSequence getPlayersPermPrefix(Player player) {
-		String prefix = BungeeSuiteChat.CHAT.getPlayerPrefix(player);
-		if(prefix == null){
-			return "";
-		}
-		return prefix;
-	}
+    public static String getPlayersGroupPrefix( Player player ) {
+        String group = BungeeSuiteChat.CHAT.getPrimaryGroup( player );
+        String prefix = "";
+        if ( !group.equals( "" ) ) {
+            prefix = BungeeSuiteChat.CHAT.getGroupPrefix( player.getWorld(), group );
+        }
+        if ( prefix == null ) {
+            return "";
+        }
+        return prefix;
+    }
 
 
-	public static String getPlayersSuffixGroup(Player player) {
-		for(String s : suffixes.keySet()){
-			if(player.hasPermission("bungeesuite.chat.suffix."+s)){
-				return s;
-			}
-		}
-		return "";
-	}
+    public static CharSequence getPlayersPermPrefix( Player player ) {
+        String prefix = BungeeSuiteChat.CHAT.getPlayerPrefix( player );
+        if ( prefix == null ) {
+            return "";
+        }
+        return prefix;
+    }
 
 
-	public static String getPlayersPrefixGroup(Player player) {
-		for(String s : prefixes.keySet()){
-			if(player.hasPermission("bungeesuite.chat.prefix."+s)){
-				return s;
-			}
-		}
-		return "";
-	}
+    public static String getPlayersSuffixGroup( Player player ) {
+        for ( String s : suffixes.keySet() ) {
+            if ( player.hasPermission( "bungeesuite.chat.suffix." + s ) ) {
+                return s;
+            }
+        }
+        return "";
+    }
 
 
-	public static String getPlayersPrefix(String group) {
-		String prefix = prefixes.get(group);
-		if(prefix==null){
-			return "";
-		}
-		return prefix;
-	}
-	public static String getPlayersSuffix(String group) {
-		String suffix = suffixes.get(group);
-		if(suffix==null){
-			return "";
-		}
-		return suffix;
-	}
+    public static String getPlayersPrefixGroup( Player player ) {
+        for ( String s : prefixes.keySet() ) {
+            if ( player.hasPermission( "bungeesuite.chat.prefix." + s ) ) {
+                return s;
+            }
+        }
+        return "";
+    }
 
 
-	
-	
+    public static String getPlayersPrefix( String group ) {
+        String prefix = prefixes.get( group );
+        if ( prefix == null ) {
+            return "";
+        }
+        return prefix;
+    }
+
+    public static String getPlayersSuffix( String group ) {
+        String suffix = suffixes.get( group );
+        if ( suffix == null ) {
+            return "";
+        }
+        return suffix;
+    }
+
+
 }
